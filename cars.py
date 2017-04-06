50b72f7ee6878e5491b2b7e323f0bd214f3b7e32
Helana <helanan@gmail.com>
2017-04-06 11:38:05 -0500

    added showroom

---
+++ cars.py
@@ -0,0 +1,6 @@
+#sets: A set is an unordered collection with no duplicate elements
+#membership testing and eliminating duplicate entries
+
+showroom = {'Acura', 'BMW', 'Honda', 'Toyota'}
+print(showroom)
+print len(showroom)
